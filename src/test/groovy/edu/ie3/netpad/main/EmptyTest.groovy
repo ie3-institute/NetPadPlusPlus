0b5bc71c746452acb1a72eec2e1179bbadc6fd3b
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-06-12 21:16:43 +0200

    fmt

--- src/test/groovy/edu/ie3/netpad/main/EmptyTest.groovy
+++ src/test/groovy/edu/ie3/netpad/main/EmptyTest.groovy
@@ -1,3 +1,8 @@
+/*
+ * © 2020. TU Dortmund University,
+ * Institute of Energy Systems, Energy Efficiency and Energy Economics,
+ * Research group Distribution grid planning and operation
+ */
 package edu.ie3.netpad.main
 
 import spock.lang.Specification
@@ -5,9 +10,8 @@ import spock.lang.Specification
 
 class EmptyTest extends Specification {
 
-    def "remove me when a first test is implemented"() {
-        expect:
-        true
-    }
-
+	def "remove me when a first test is implemented"() {
+		expect:
+		true
+	}
 }
