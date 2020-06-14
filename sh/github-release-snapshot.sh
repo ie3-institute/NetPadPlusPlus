33d962759c6932e9066344a445a8823ae6999365
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-06-14 15:37:27 +0200

    minor style improvements in github-release-snapshot.sh

--- sh/github-release-snapshot.sh
+++ sh/github-release-snapshot.sh
@@ -8,7 +8,7 @@ version=v$(../gradlew -q printVersion)
 
 # check if release exists for this tag
 ## get the id if any 
-id=$(curl -s -XGET -H "Authorization:token $token" https://api.github.com/repos/ie3-institute/NetPadPlusPlus/releases/tags/$version  | grep -o -E "\"id\":\s[0-9]+" | awk 'NR==1{gsub(/^[ \t]+| [ \t]+$/,"");print $2}')
+id=$(curl -s -XGET -H "Authorization:token $token" https://api.github.com/repos/ie3-institute/NetPadPlusPlus/releases/tags/${version}  | grep -o -E "\"id\":\s[0-9]+" | awk 'NR==1{gsub(/^[ \t]+| [ \t]+$/,"");print $2}')
 
 if [[ -z "$id" ]]
 then
@@ -20,7 +20,7 @@ fi
 
 # create new release 
 ## get the full message associated with the current tag
-message="$(git for-each-ref refs/tags/$version --format='%(contents)')"
+message="$(git for-each-ref refs/tags/${version} --format='%(contents)')"
 
 ## get the title and the description as separated variables
 name=$(echo "$message" | head -n1)
@@ -32,7 +32,7 @@ release=$(curl -s -XPOST -H "Authorization:token $token" --data "{\"tag_name\": 
 
 ## Extract the id of the release from the creation response
 echo "old release id: $id"
-id=$(curl -s -XGET -H "Authorization:token $token" https://api.github.com/repos/ie3-institute/NetPadPlusPlus/releases/tags/$version  | grep -o -E "\"id\":\s[0-9]+" | awk 'NR==1{gsub(/^[ \t]+| [ \t]+$/,"");print $2}')
+id=$(curl -s -XGET -H "Authorization:token $token" https://api.github.com/repos/ie3-institute/NetPadPlusPlus/releases/tags/${version}  | grep -o -E "\"id\":\s[0-9]+" | awk 'NR==1{gsub(/^[ \t]+| [ \t]+$/,"");print $2}')
 
 echo "new release id: $id"
 
@@ -48,7 +48,7 @@ zip -jr NetPadPlusPlus.zip build/libs/
 
 ## upload
 echo 'uploading artifact ...'
-curl -XPOST -H "Authorization:token $token" -H "Content-Type: application/zip" --data-binary @NetPadPlusPlus.zip https://uploads.github.com/repos/ie3-institute/NetPadPlusPlus/releases/$id/assets?name=NetPadPlusPlus.zip
+curl -XPOST -H "Authorization:token $token" -H "Content-Type: application/zip" --data-binary @NetPadPlusPlus.zip https://uploads.github.com/repos/ie3-institute/NetPadPlusPlus/releases/${id}/assets?name=NetPadPlusPlus.zip
 
 ## cleanup
 rm NetPadPlusPlus.zip 2> /dev/null
